e324532f16878426e5cec0add34403e33237fc14
Masaya Kataoka <ms.kataoka@gmail.com>
2022-10-07 21:44:54 +0900

    remain compile error
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- battery_monitor/templates.hpp
+++ battery_monitor/templates.hpp
@@ -1,6 +1,4 @@
 #include "std_msgs/msg/u_int8.hpp"
 
-template mros2::Subscriber mros2::Node::create_subscription(
-  std::string topic_name, int qos, void (*fp)(std_msgs::msg::UInt8 *));
-template void mros2::Subscriber::callback_handler<std_msgs::msg::UInt8>(
-  void * callee, const rtps::ReaderCacheChange & cacheChange);
+template mros2::Publisher mros2::Node::create_publisher<std_msgs::msg::Uint8>(std::string topic_name, int qos);
+template void mros2::Publisher::publish(std_msgs::msg::Uint8 &msg);
