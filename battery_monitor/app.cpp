1f85b29a35d5190b66bfdd76d86cf6660a1d781b
Masaya Kataoka <ms.kataoka@gmail.com>
2022-10-07 21:36:43 +0900

    remain compile error
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- battery_monitor/app.cpp
+++ battery_monitor/app.cpp
@@ -1,7 +1,19 @@
+#include "EthernetInterface.h"
 #include "mbed.h"
 #include "mros2.h"
+#include "std_msgs/msg/u_int8.hpp"
+
+#define IP_ADDRESS ("192.168.11.2")      /* IP address */
+#define SUBNET_MASK ("255.255.255.0")    /* Subnet mask */
+#define DEFAULT_GATEWAY ("192.168.11.1") /* Default gateway */
 
 int main()
 {
-    
+  EthernetInterface network;
+  network.set_dhcp(false);
+  network.set_network(IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY);
+  nsapi_size_or_error_t result = network.connect();
+  mros2::init(0, NULL);
+  mros2::Node node = mros2::Node::create_node("battery_monitor_node");
+  mros2::Publisher pub = node.create_publisher<std_msgs::msg::Unit8>("battery_state", 1);
 }
--- battery_monitor/templates.hpp
+++ battery_monitor/templates.hpp
@@ -1,4 +1,6 @@
 #include "std_msgs/msg/u_int8.hpp"
 
-template mros2::Subscriber mros2::Node::create_subscription(std::string topic_name, int qos, void (*fp)(std_msgs::msg::UInt8*));
-template void mros2::Subscriber::callback_handler<std_msgs::msg::UInt8>(void *callee, const rtps::ReaderCacheChange &cacheChange);
+template mros2::Subscriber mros2::Node::create_subscription(
+  std::string topic_name, int qos, void (*fp)(std_msgs::msg::UInt8 *));
+template void mros2::Subscriber::callback_handler<std_msgs::msg::UInt8>(
+  void * callee, const rtps::ReaderCacheChange & cacheChange);
